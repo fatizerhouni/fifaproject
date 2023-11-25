e9c24501df9dda48daed224fe0fceb94a5057af5
benthami <f.benthami@gmail.com>
2023-11-25 12:21:21 +0100

    add my files

---
+++ fifa-player-cards/src/Component/Players.js
@@ -0,0 +1,19 @@
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Paris Saint-Germain",
+      nationality: "Argentina",
+      jerseyNumber: 30,
+      age: 34,
+      imageUrl: "https://images.unsplash.com/photo-1671016233730-44258a88eb03?q=80&w=2033&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "Cristiano Ronaldo",
+        team: "Manchester United",
+        nationality: "Portugais",
+        jerseyNumber: 7,
+        age: 36,
+        imageUrl: "https://images.unsplash.com/photo-1671016233730-44258a88eb03?q=80&w=2033&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+  ];
+  export default players;
